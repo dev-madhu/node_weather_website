46fdf4879d438cbdbb63f51fa7222594446e5cb3
Kanukula, Madhu <madhu1@hpe.com>
2021-04-05 14:10:30 +0530

    remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side Javascript loaded")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 let message1 = document.querySelector('#message-1')
