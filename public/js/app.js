78232da0f3ab008541579f72509ccba3c5229374
Kanukula, Madhu <madhu1@hpe.com>
2021-04-05 14:06:35 +0530

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log("Client side Javascript loaded")
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+let message1 = document.querySelector('#message-1')
+let message2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+    
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+            } else {
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
+    })
+})
